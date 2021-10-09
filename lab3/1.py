510ecd9bd0856d2ecd9772afbaf2b9f99643e61b
Daniil <the.real.daniil17@gmail.com>
2021-10-09 15:45:25 +0300

    lab3, task 1

---
+++ lab3/1.py
@@ -0,0 +1,46 @@
+# методом Ньютона решить нелинейное уравнение ln(x) − 1/x^2 = 0
+
+from sympy.solvers import solve
+from sympy import Symbol, log, diff
+from sympy.utilities.lambdify import lambdify
+from random import uniform
+import numpy as np
+import matplotlib.pyplot as plt
+
+# погрешность
+EPS = 0.0001
+
+# задаем символьно функцию
+x = Symbol('x')
+expr = log(x) - 1/x/x
+
+# рисуем график
+x_range = np.linspace(0.1, 20, 100)
+y_range = lambdify(x, expr)(x_range)
+plt.plot(x_range, y_range)
+plt.show()
+
+# находим точные корни
+root = solve(expr, x)[0].evalf()
+print('Exact root: ', root)
+
+
+# находит корни методом Ньютона с погрешностью EPS
+def solve_by_newton_method(expr, a, b):
+    f = lambdify(x, expr)
+    df = lambdify(x, diff(expr))
+    x0 = uniform(a, b)
+    if df(a) * f(x0) > 0:
+        x0 = a
+    else:
+        x0 = b
+    x1 = x0 - f(x0)/df(x0)
+    while abs(x1 - x0) >= EPS:
+        x0 = x1
+        x1 = x0 - f(x0) / df(x0)
+    return x1
+
+
+newton_root = solve_by_newton_method(expr, 0.1, 10)
+print('Newton root: ', newton_root)
+print('Error value: ', abs(root - newton_root))
